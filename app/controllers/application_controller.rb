6f4e96720dfa32863bd1e4724845820895e3fb5d
Saad786 <shahid.saad76@gmail.com>
2018-07-27 11:29:06 +0000

    Sample Commit message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_type])
+    puts 'xx'
     end 
 
     #this method is pundit builten method for user authorizations
